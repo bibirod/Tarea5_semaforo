65f67053f9a2984e2a2eb251b3ea5f90df68de38
BibianaR <nbibiro@gmail.com>
2023-06-08 19:47:25 -0500

    version final con timeout

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 
-function cambio_color(){
+function cambio_color() {
     let color;
-    color= "rojo";
-    alert(`el semaforo está en ${color}`);
-    document.getElementById("rojo").hidden=false;
+    color = "rojo";
+    //alert(`el semaforo está en ${color}`);
+    document.getElementById("rojo").hidden = false;
+    setTimeout(() => {
+        if (color == "rojo") {
+            color = "verde";
+            //alert(`el semaforo está en ${color}`);
+            document.getElementById("rojo").hidden = true;
+            document.getElementById("verde").hidden = false;
 
-        if(color=="rojo"){
-            color="verde";
-            alert(`el semaforo está en ${color}`);
-            document.getElementById("rojo").hidden=true;
-            
-            document.getElementById("verde").hidden=false;
-                         
-            
-       }
+        }
+        setTimeout(() => {
+            if (color == "verde") {
+                color = "amarillo";
+                // alert(`el semaforo está en ${color}`);
+                document.getElementById("verde").hidden = true;
 
-    if(color=="verde"){
-        color="amarillo";
-        alert(`el semaforo está en ${color}`);
-        document.getElementById("verde").hidden=true;
-            
-        document.getElementById("amarillo").hidden=false;
+                document.getElementById("amarillo").hidden = false;
+            }
 
-    }
+        }, 5000);
+
+    }, 5000);
+
+  
+
+
 }
-    alert("Inicia semaforo");
-    cambio_color();
-    
+alert("Inicia semaforo");
+cambio_color();
+
